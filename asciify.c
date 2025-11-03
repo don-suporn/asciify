7ef6a58d42d9f894eb6af97b7ada754c8984e10d
donsu <don.suporns@gmail.com>
2025-11-03 19:17:03 +0700

    added gitignore

--- asciify.c
+++ asciify.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <wchar.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "includes\stb_image.h"
 
 int main(int argc, char *argv[]) {
     // Check for invalid usage
