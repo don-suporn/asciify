bd14b1914275193f3e48002c2c562adeb03e2d49
don-suporns <don.suporns@gmail.com>
2025-11-08 17:05:18 +0700

    added colour shading

--- asciify.c
+++ asciify.c
@@ -6,19 +6,23 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 #include <wchar.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "includes/stb_image.h"
+
+int getColorCode(char* colorName);
 
 int main(int argc, char *argv[]) {
     // Handle options
     int opt;
     char* imagePath = "";
     int asciiHeight = 30;
+    int colorCode;
 
     opterr = 0;
-    while ((opt = getopt(argc, argv, "h:")) != -1) {
+    while ((opt = getopt(argc, argv, ":h:c:")) != -1) {
         switch (opt)
         {
             case 'h':
@@ -28,6 +32,15 @@ int main(int argc, char *argv[]) {
                     return 1;
                 }
                 break;
+            case 'c':
+                colorCode = getColorCode(optarg);
+                if (colorCode == 0) {
+                    colorCode = 37;
+                }
+                break;
+            case ':':
+                printf("Option -%c requires an argument\n", optopt);
+                return 1;
             case '?':
                 printf("Unknown option -%c\n", optopt);
                 return 1;
@@ -107,30 +120,67 @@ int main(int argc, char *argv[]) {
     // ▒
     // ░
 
+    char* lightestShade = "█";
+    char* lightShade = "▓";
+    char* lightishShade = "▒";
+    char* darkShade = "░";
+    char* darkestShade = " ";
+
+    printf("%i", colorCode);
+    printf("\033[0;%im", colorCode);
+
     // Print!!
     for (int asciiRow = 0; asciiRow < asciiHeight; asciiRow += 1) {
         for (int asciiColumn = 0; asciiColumn < asciiWidth; asciiColumn += 1) {
             int currentBlock = asciiArray[asciiRow][asciiColumn];
             if (currentBlock >= shadeUpperBound - shadeRange / 4) {
-                printf("█");
+                printf("%s", lightestShade);
             }
             else if (currentBlock >= shadeUpperBound - shadeRange / 2) {
-                printf("▓");
+                printf("%s", lightShade);
             }
             else if (currentBlock >= shadeUpperBound - shadeRange * 7 / 10) {
-                printf("▒");
+                printf("%s", lightishShade);
             }
             else if (currentBlock >= shadeUpperBound - shadeRange * 4 / 5) {
-                printf("░");
+                printf("%s", darkShade);
             }
             else {
-                printf(" ");    
+                printf("%s", darkestShade);    
             }
         }
         printf("\n");
     }
-    
+    printf("\033[0m");
+
     // Free image data
     stbi_image_free(data);
 
+}
+
+int getColorCode(char* colorName) {
+    if (strcmp(colorName, "black") == 0) {
+        return 30;
+    }
+    else if (strcmp(colorName, "red") == 0) {
+        return 31;
+    }
+    else if (strcmp(colorName, "green") == 0) {
+        return 32;
+    }
+    else if (strcmp(colorName, "yellow") == 0) {
+        return 33;
+    }
+    else if (strcmp(colorName, "blue") == 0) {
+        return 34;
+    }
+    else if (strcmp(colorName, "magenta") == 0) {
+        return 35;
+    }
+    else if (strcmp(colorName, "cyan") == 0) {
+        return 36;
+    }
+    else {
+        return 0;
+    }
 }
